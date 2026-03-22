8e96c741a9dd469be5281f7b3049e35b20eae819
root <root@localhost.localdomain>
2026-03-22 22:04:01 +0800

    chore: change port to 4888

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 4888,
+    allowedHosts: ['qlwy.groux.top'],
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
